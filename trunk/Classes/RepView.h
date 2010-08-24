dbfeb047fa3b081ae27ed69ff958c723e16a95e0
JamesRPurser <JamesRPurser@6b30ff30-9db9-2f0e-6c25-73b2022f0c21>
2010-08-24 02:44:58 +0000

    Added basic classes and very basic gui work for main view.

---
+++ trunk/Classes/RepView.h
@@ -0,0 +1,16 @@
+//
+//  RepView.h
+//  openaustraliasearch
+//
+//  Created by James Purser on 24/08/10.
+//  Copyright 2010 James Purser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RepView : UIViewController {
+
+}
+
+@end
